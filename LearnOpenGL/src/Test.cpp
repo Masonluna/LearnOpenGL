3bc39a51c7109eb3e02616304ecb21bbd229f1df
Mason Luna <mason.luna24@yahoo.com>
2024-08-07 21:28:57 -0500

    Baby's first triangle :)

--- LearnOpenGL/src/Test.cpp
+++ LearnOpenGL/src/Test.cpp
@@ -7,8 +7,27 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+const unsigned int SCR_WIDTH = 640;
+const unsigned int SCR_HEIGHT = 360;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 int main()
 {
+	// ========== INIT ==========
+
 	// Initialize GLFW and set the version to 3
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -16,41 +35,124 @@ int main()
 	// Make sure the OpenGL version that we are using is the core profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
+	// Vertex Data
 	// Let's Make a window!
-	GLFWwindow* window = glfwCreateWindow(640, 360, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (!window) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window); // Must be done before GLAD is initialized
+	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
 
 	// Initialize GLAD; NOTE: must make glfw context current before initializing GLAD. Order matters here.
 	// Pass GLAD the function to load the OpenGL function pointers, glfwGetProcAddress defines the correct function based on OS
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { 
 		std::cout << "Failed to initialize GLAD" << std::endl;
 	}
-
-	// Tell OpenGL the window size
-	// (0,0) == top-left pixel
-	// (640, 360) == bottom-right pixel
-	// (x == -1.0) -> leftmost pixel		(x == 1.0) -> rightmost pixel
-	// (y == -1.0) -> top pixel				(y == 1.0) -> bottom pixel
-	glViewport(0, 0, 640, 360);
 	
 
+	// ========== GRAPHICS PIPELINE ==========
+	// ---------- VERTEX DATA ----------
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+
+	// Vertex Buffer Object Generation
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); 	// Bind the VBO to it's correct buffer type
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 	// copy the vertex data into GL_ARRAY_BUFFER
+	// GL_STREAM_DRAW: data is set once and used only a few times
+	// GL_STATIC_DRAW: data is set once and used many times
+	// GL_DYNAMIC_DRAW: data is often changed and used many times
+	// These determine where the GPU places the data in memory
+
+	// ---------- VERTEX SHADER ----------
+	// Vertex Shader Compilation
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Verify vertexShader compiled Correctly
+	int success;
+	char infoLog[512]; // Why 512? It's the size of the buffer
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// ---------- FRAGMENT SHADER ----------
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// ---------- SHADER PROGRAM ----------
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	// Attach Shaders
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Verify success
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Delete shaders once they're linked to the shader program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	// Tell OpenGL how to interpret the vertex data
+	//                  LOC SIZ  TYPE     NORMALIZE?  SPACE BETWEEN ATT  OFFSET
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// ---------- VERTEX ARRAY OBJECTS ----------
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	// ========== RENDERING ==========
 	// Set up Render loop
 	while (!glfwWindowShouldClose(window)) {
 		// 1.) input
 		processInput(window);
 
 		// 2.) rendering commands
-		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClearColor(0.4f, 0.8f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// OpenGl will now use shaderProgram for any rendering
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// 3.) check and call events and swap the buffers
 		glfwSwapBuffers(window);
